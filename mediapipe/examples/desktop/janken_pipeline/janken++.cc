795de3c4978e7527e44eeaf6f4bd63d903e9ab83
ksato-vrain <sato.kazuki@kyla.co.jp>
2022-08-20 16:57:18 +0900

    Fixed display.

--- mediapipe/examples/desktop/janken_pipeline/janken++.cc
+++ mediapipe/examples/desktop/janken_pipeline/janken++.cc
@@ -18,6 +18,10 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <random>
+#include <fstream>
+#include <locale.h>
+#include <wchar.h>
 
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
@@ -50,6 +54,32 @@ constexpr char kInputStream[] = "input_video";
 constexpr char kOutputStream[] = "landmarks";
 constexpr char kWindowName[] = "Janken++";
 
+const std::vector<std::vector<int>> kConnectionList = {
+  {0, 1}, {1, 2}, {2, 3}, {3, 4},
+  {0, 5}, {5, 6}, {6, 7}, {7, 8},
+  {5, 9},
+  {9, 10}, {10, 11}, {11, 12},
+  {9, 13},
+  {13, 14}, {14, 15}, {15, 16},
+  {13, 17},
+  {17, 18}, {18, 19}, {19, 20},
+  {0, 17},
+};
+
+// 白画像を作る関数
+void CreateWhiteImage(const cv::Size &size, cv::Mat *output_image) {
+  *output_image = cv::Mat::zeros(size, CV_8UC3);
+  int cols = output_image->cols;
+  int rows = output_image->rows;
+  for (int j = 0; j < rows; j++) {
+    for (int i = 0; i < cols; i++) {
+      output_image->at<cv::Vec3b>(j, i)[0] = 255; // 青
+      output_image->at<cv::Vec3b>(j, i)[1] = 255; // 緑
+      output_image->at<cv::Vec3b>(j, i)[2] = 255; // 赤
+    }
+  }
+}
+
 // 画像を画像に貼り付ける関数
 // ref: https://kougaku-navi.hatenablog.com/entry/20160108/p1
 void Overlap(cv::Mat dst, cv::Mat src, int x, int y, int width, int height) {
@@ -70,33 +100,22 @@ void Overlap(cv::Mat dst, cv::Mat src, int x, int y, int width, int height) {
 }
 
 void DrawNodePoints(const mediapipe::NormalizedLandmarkList &landmarks,
-                    const cv::Mat &camera_frame_raw, cv::Mat *output_frame_mat) {
+                    const cv::Mat &camera_frame_raw, cv::Mat *output_frame_display_right) {
   for (int j = 0; j < landmarks.landmark_size(); j++) {
     auto &landmark = landmarks.landmark(j);
     int x = int(std::round(landmark.x() * camera_frame_raw.cols));
     int y = int(std::round(landmark.y() * camera_frame_raw.rows));
     // std::cout << "x, y = " << x << ", " << y
     //           << std::endl;
-    cv::circle(*output_frame_mat, cv::Point(x, y), 2, cv::Scalar(0, 0, 255), 4,
+    cv::circle(*output_frame_display_right, cv::Point(x, y), 2, cv::Scalar(0, 0, 255), 4,
                cv::LINE_4);
   }
 }
 
 void DrawFrameLines(const mediapipe::NormalizedLandmarkList &landmarks,
-                    const cv::Mat &camera_frame_raw, cv::Mat *output_frame_mat) {
-  const std::vector<std::vector<int>> connection_list = {
-    {0, 1}, {1, 2}, {2, 3}, {3, 4},
-    {0, 5}, {5, 6}, {6, 7}, {7, 8},
-    {5, 9},
-    {9, 10}, {10, 11}, {11, 12},
-    {9, 13},
-    {13, 14}, {14, 15}, {15, 16},
-    {13, 17},
-    {17, 18}, {18, 19}, {19, 20},
-    {0, 17},
-  };
+                    const cv::Mat &camera_frame_raw, cv::Mat *output_frame_display_right) {
   // index
-  for (auto &conn : connection_list) {
+  for (auto &conn : kConnectionList) {
     auto &landmark1 = landmarks.landmark(conn[0]);
     int x1 = int(std::round(landmark1.x() * camera_frame_raw.cols));
     int y1 = int(std::round(landmark1.y() * camera_frame_raw.rows));
@@ -105,7 +124,7 @@ void DrawFrameLines(const mediapipe::NormalizedLandmarkList &landmarks,
     int x2 = int(std::round(landmark2.x() * camera_frame_raw.cols));
     int y2 = int(std::round(landmark2.y() * camera_frame_raw.rows));
 
-    cv::line(*output_frame_mat, cv::Point(x1, y1), cv::Point(x2, y2),
+    cv::line(*output_frame_display_right, cv::Point(x1, y1), cv::Point(x2, y2),
              cv::Scalar(0, 255, 0), 4, cv::LINE_4);
   }
 }
@@ -130,6 +149,43 @@ absl::Status RunMPPGraph(
     // const cv::Mat &camera_frame_raw,
     std::vector<std::vector<cv::Point2i>> *left_eye_landmarks_list,
     std::vector<std::vector<cv::Point2i>> *right_eye_landmarks_list) {
+
+  std::vector<std::string> kThemeMsgList(0);
+#if 1
+  // なぜか下記が実行できなくてキレそう（キレてる）
+  // kThemeMsgList.push_back(std::string("に勝て！"));
+  // kThemeMsgList.push_back(std::string("に負けろ！"));
+  // kThemeMsgList.push_back(std::string("とあいこ！"));
+  // kThemeMsgList.push_back(std::string("aaa"));  // 英語はいける。やっぱ日本語はクソ
+
+  // 英語版
+  kThemeMsgList.push_back(std::string(": Win this gesture!"));
+  kThemeMsgList.push_back(std::string(": Lose this gesture!"));
+  kThemeMsgList.push_back(std::string(": Draw this gesture!"));
+#else
+  // ref: https://code-database.com/knowledges/123
+  std::ifstream ifs;
+  ifs.open("mediapipe/examples/desktop/janken_pipeline/msg_list.txt", std::ios::in);
+  std::string reading_line_buffer;
+  setlocale(LC_CTYPE, "ja_JP.UTF-8");
+
+  while(std::getline(ifs, reading_line_buffer)){
+      size_t capacity = strlen(reading_line_buffer.c_str()) + 1;
+      wchar_t *str2 = (wchar_t *)malloc(sizeof(wchar_t) * capacity);
+
+      // char -> wchar_tの変換
+      int result = mbstowcs(str2, reading_line_buffer.c_str(), capacity);
+
+      std::cout << str2 << std::endl;
+      // kThemeMsgList.push_back(reading_line_buffer);
+  }
+#endif
+
+  std::random_device rnd; // 非決定的な乱数生成器
+  std::mt19937_64 mt(rnd()); // メルセンヌ・ツイスタの32ビット版、引数は初期シード
+  std::uniform_int_distribution<> rand_n(
+      0, kThemeMsgList.size() - 1); // [0, n] 範囲の一様乱数
+
   // std::cout << "called RunMPPGraph" << std::endl;
   mediapipe::CalculatorGraphConfig config;
   Configure(calculator_graph_config_file, &config);
@@ -164,8 +220,6 @@ absl::Status RunMPPGraph(
   }
   MP_RETURN_IF_ERROR(graph.StartRun({}));
 
-  // std::cout << "Start grabbing and processing frames." << std::endl;
-
   cv::VideoCapture capture;
   capture.open(0);
 
@@ -180,9 +234,6 @@ absl::Status RunMPPGraph(
       MP_RETURN_IF_ERROR(graph.CloseInputStream(kInputStream));
       return graph.WaitUntilDone();
     }
-    // std::cout << "exec once." << std::endl;
-    // continue;
-    // cv::Mat camera_frame_raw = cv::imread(camera_frame_raw_path);
     cv::Mat camera_frame;
     cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
     // cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
@@ -208,71 +259,116 @@ absl::Status RunMPPGraph(
 
     graph.WaitUntilIdle();
 
-    cv::Mat output_frame_mat = cv::Mat::zeros(camera_frame_raw.size(), CV_8UC3);
-    // cv::Mat output_frame_mat;
-    // camera_frame_raw.copyTo(output_frame_mat);
+    cv::Mat landmark_image = cv::Mat::zeros(camera_frame_raw.size(), CV_8UC3);
 
+    // PostProcess ---
     for (int i = 0; i < landmarks_list.size(); i++) {
       mediapipe::NormalizedLandmarkList &landmarks = landmarks_list[i];
-      // std::cout << "num_of_landmarks:" << landmarks.landmark_size()
-      //           << std::endl;
-
-      // const int num_refined_landmarks = 478;
-
       // draw frame-lines
-      DrawFrameLines(landmarks, camera_frame_raw, &output_frame_mat);
+      DrawFrameLines(landmarks, camera_frame_raw, &landmark_image);
 
       // draw node-points
-      DrawNodePoints(landmarks, camera_frame_raw, &output_frame_mat);
-
-      // break;  // 片方の手だけ利用する。
+      DrawNodePoints(landmarks, camera_frame_raw, &landmark_image);
     }
 
+    cv::Mat output_frame_display_right;
+    output_frame_display_right = cv::Mat::zeros(
+        cv::Size(camera_frame_raw.rows, camera_frame_raw.rows), CV_8UC3);
+
+    // TODO: refactor
     GestureType pre_recognized_type = GestureType::UNKNOWN;
     if (landmarks_list.size() == 1) {
       // 片手
+      cv::Mat gesture_image;
       for (auto &estimator : one_hand_estimator_list) {
         // std::cout << landmarks_list[0].landmark_size() << std::endl;
         estimator->Initialize();
         const GestureType recognized_type = estimator->Recognize(landmarks_list);
         if (recognized_type != GestureType::UNKNOWN) {
-          // std::cout << "Gesture-type: " << recognized_type << std::endl;
-          std::string print_msg = "Gesture ID: " + std::to_string(recognized_type);
-          if (pre_recognized_type == GestureType::UNKNOWN)
-            cv::putText(output_frame_mat, print_msg, cv::Point(10, 30), 2, 1.0,
-                        cv::Scalar(0, 255, 0), 2, cv::LINE_4);
-          
+          if (recognized_type == GestureType::GU)
+            gesture_image = cv::imread("mediapipe/resources/gu.png");
+          else if (recognized_type == GestureType::CHOKI)
+            gesture_image = cv::imread("mediapipe/resources/choki.png");
+          else if (recognized_type == GestureType::PA)
+            gesture_image = cv::imread("mediapipe/resources/pa.png");
+
           pre_recognized_type = recognized_type;
         }
       }
+      if (gesture_image.empty()) {
+        gesture_image = cv::Mat::zeros(
+            cv::Size(camera_frame_raw.rows, camera_frame_raw.rows), CV_8UC3);
+      }
+      else {
+        cv::resize(gesture_image, gesture_image,
+                   cv::Size(camera_frame_raw.rows, camera_frame_raw.rows));
+      }
+      Overlap(gesture_image, landmark_image, -3,
+              camera_frame_raw.rows - 105,
+              std::roundl(camera_frame_raw.cols * 0.2),
+              std::roundl(camera_frame_raw.rows * 0.2));
+      output_frame_display_right = gesture_image;
     }
     else if (landmarks_list.size() == 2) {
       // 両手
+      cv::Mat gesture_image;
       for (auto &estimator : two_hands_estimator_list) {
-        // std::cout << landmarks_list[0].landmark_size() << std::endl;
         estimator->Initialize();
         const GestureType recognized_type = estimator->Recognize(landmarks_list);
         if (recognized_type != GestureType::UNKNOWN) {
-          // std::cout << "Gesture-type: " << recognized_type << std::endl;
-          std::string print_msg = "Gesture ID: " + std::to_string(recognized_type);
-          if (pre_recognized_type == GestureType::UNKNOWN)
-            cv::putText(output_frame_mat, print_msg, cv::Point(10, 30), 2, 1.0,
-                        cv::Scalar(0, 255, 0), 2, cv::LINE_4);
-
-          if (recognized_type == GestureType::HEART) {
-            const cv::Mat overlap_image = cv::imread("mediapipe/resources/heart.png");
-            Overlap(output_frame_mat, overlap_image, 10, camera_frame_raw.rows - 55, 45, 45);
-          }
+          if (recognized_type == GestureType::HEART)
+            gesture_image = cv::imread("mediapipe/resources/heart.png");
 
           pre_recognized_type = recognized_type;
         }
       }
+      if (gesture_image.empty()) {
+        gesture_image = cv::Mat::zeros(
+            cv::Size(camera_frame_raw.rows, camera_frame_raw.rows), CV_8UC3);
+      }
+      else {
+        cv::resize(gesture_image, gesture_image,
+                   cv::Size(camera_frame_raw.rows, camera_frame_raw.rows));
+      }
+      Overlap(gesture_image, landmark_image, -3,
+              camera_frame_raw.rows - 105,
+              std::roundl(camera_frame_raw.cols * 0.2),
+              std::roundl(camera_frame_raw.rows * 0.2));
+      output_frame_display_right = gesture_image;
     }
-
     landmarks_list = std::vector<mediapipe::NormalizedLandmarkList>();  // reset
 
-    // cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
-    cv::imshow(kWindowName, output_frame_mat);
+    cv::Mat output_frame_display;
+
+    // 左の表示 ---
+    // cv::Mat output_frame_display_left = cv::Mat::zeros(camera_frame_raw.size(), CV_8UC3);
+    // cv::Mat output_frame_display_left = cv::Mat::zeros(
+    //     cv::Size(camera_frame_raw.rows, camera_frame_raw.rows), CV_8UC3);
+    // int cols = output_frame_display_left.cols;
+    // int rows = output_frame_display_left.rows;
+    // for (int j = 0; j < rows; j++) {
+    //     for (int i = 0; i < cols; i++) {
+    //         output_frame_display_left.at<cv::Vec3b>(j, i)[0] = 255; //青
+    //         output_frame_display_left.at<cv::Vec3b>(j, i)[1] = 255; //緑
+    //         output_frame_display_left.at<cv::Vec3b>(j, i)[2] = 255; //赤
+    //     }
+    // }
+
+    cv::Mat output_frame_display_left;
+    // CreateWhiteImage(cv::Size(camera_frame_raw.rows, camera_frame_raw.rows),
+    //                  &output_frame_display_left);
+    output_frame_display_left = cv::Mat::zeros(
+        cv::Size(camera_frame_raw.rows, camera_frame_raw.rows), CV_8UC3);
+
+    cv::putText(output_frame_display_left, kThemeMsgList[rand_n(mt)], cv::Point(10, 30), 2, 1.0,
+                cv::Scalar(0, 255, 0), 2, cv::LINE_4);
+    // --- 左の表示
+
+    cv::hconcat(output_frame_display_left, output_frame_display_right, output_frame_display);
+    // --- PostProcess
+
+    // cv::cvtColor(output_frame_display_right, output_frame_display_right, cv::COLOR_RGB2BGR);
+    cv::imshow(kWindowName, output_frame_display);
     // Press any key to exit.
     const int pressed_key = cv::waitKey(1);
     if (pressed_key >= 0 && pressed_key != 255) grab_frames = false;
